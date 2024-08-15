95ae916dc1df47892065a4e007bf8c9e58626be8
Bianca Sanches <biancasanches.dev@gmail.com>
2024-08-15 18:09:45 -0300

    feat: tratamento de erros e interceptor para autenticacao

--- src/main/java/com/example/hospital/controller/DashboardController.java
+++ src/main/java/com/example/hospital/controller/DashboardController.java
@@ -1,9 +1,7 @@
 package com.example.hospital.controller;
 
 import com.example.hospital.model.dashboard.DashboardData;
-import com.example.hospital.model.usuario.Usuario;
 import com.example.hospital.service.*;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,20 +16,10 @@ public class DashboardController {
         this.dashboardService = dashboardService;
     }
 
-//    private void getData(Model model) {
-//        DashboardData data = dashboardService.getDashboardData();
-//        model.addAttribute("data", data);
-//    }
-
     @GetMapping
-    public String getDashboard(Model model, HttpSession session) {
-        Usuario usuario = (Usuario) session.getAttribute("user");
-        if (usuario == null) {
-            return "redirect:/login";
-        }
+    public String getDashboard(Model model) {
         DashboardData data = dashboardService.getDashboardData();
         model.addAttribute("data", data);
-        model.addAttribute("usuario", usuario);
         return "home";
     }
 
--- src/main/java/com/example/hospital/controller/IncidenteController.java
+++ src/main/java/com/example/hospital/controller/IncidenteController.java
@@ -3,15 +3,12 @@ package com.example.hospital.controller;
 import com.example.hospital.model.dashboard.DashboardData;
 import com.example.hospital.model.incidente.Incidente;
 import com.example.hospital.model.solicitacao.Solicitacao;
-import com.example.hospital.model.usuario.Usuario;
 import com.example.hospital.service.DashboardService;
 import com.example.hospital.service.IncidenteService;
 import com.example.hospital.service.SolicitacaoService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/incidentes")
@@ -28,57 +25,38 @@ public class IncidenteController {
     }
 
     @GetMapping
-    public String getIncidentes(Model model, HttpSession session) {
-        Usuario usuario = (Usuario) session.getAttribute("user");
-        if (usuario == null) {
-            return "redirect:/login";
-        }
+    public String getIncidentes(Model model) {
         DashboardData data = dashboardService.getDashboardData();
         model.addAttribute("data", data);
-        model.addAttribute("usuario", usuario);
         return "incidentes";
     }
 
     @GetMapping("/{id}")
-    public String getIncidente(@PathVariable Long id, Model model, HttpSession session) {
+    public String getIncidente(@PathVariable Long id, Model model) {
         Incidente incidente = incidenteService.getIncidenteById(id);
-        Usuario usuario = (Usuario) session.getAttribute("user");
-        if (usuario == null) {
-            return "redirect:/login";
-        }
         DashboardData data = dashboardService.getDashboardData();
         model.addAttribute("data", data);
-        model.addAttribute("usuario", usuario);
         model.addAttribute("incidente", incidente);
         return "incidente";
     }
 
 
     @GetMapping("/solicitacao/{id}/relatar")
-    public String getRelatarIncidente(@PathVariable Long id, Model model, HttpSession session) {
-        Usuario usuario = (Usuario) session.getAttribute("user");
+    public String getRelatarIncidente(@PathVariable Long id, Model model) {
         Solicitacao solicitacao = solicitacaoService.getSolicitacaoById(id);
-        if (usuario == null) {
-            return "redirect:/login";
-        }
-        model.addAttribute("usuario", usuario);
         model.addAttribute("solicitacao", solicitacao);
         model.addAttribute("incidente", new Incidente());
         return "relatarIncidente";
     }
 
     @PostMapping("/solicitacao/{id}/relatar")
-    public String cadastrarIncidente(@PathVariable Long id, @ModelAttribute Incidente incidente, Model model, RedirectAttributes redirectAttributes) {
-        try {
-            Solicitacao solicitacao = solicitacaoService.getSolicitacaoById(id);
-            incidente.setSolicitacao(solicitacao);
-            incidenteService.saveIncidente(incidente);
-            DashboardData data = dashboardService.getDashboardData();
-            model.addAttribute("data", data);
-            redirectAttributes.addFlashAttribute("successMessage", "Solicitação enviada com sucesso!");
-        } catch (Exception e) {
-            redirectAttributes.addFlashAttribute("errorMessage", "Erro ao enviar a solicitação.");
-        }
+    public String cadastrarIncidente(@PathVariable Long id, @ModelAttribute Incidente incidente, Model model) {
+        Solicitacao solicitacao = solicitacaoService.getSolicitacaoById(id);
+        incidente.setSolicitacao(solicitacao);
+        incidenteService.saveIncidente(incidente);
+        DashboardData data = dashboardService.getDashboardData();
+        model.addAttribute("data", data);
+
         return "redirect:/incidentes";
     }
 
